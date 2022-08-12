ad091a7d151bc4d4addb4de27611cf851637b6c4
William957-web <85293841+William957-web@users.noreply.github.com>
2022-08-12 10:22:29 +0800

    Update 3.cpp

--- mockapcs202109/3.cpp
+++ mockapcs202109/3.cpp
@@ -4,15 +4,13 @@ using namespace std;
 #define ll long long
 int n;
 int ans;
-int k[300000];
-ll mn, suml, sumr;
+int k[300001], dp[300001];
+ll mn;
 int mid;
 void run(int l, int r){
 	if(l==r) ans=k[l];
 	else{
 	mn=0x6f6f6f6f;
-	suml=0;
-	sumr=0;
 	int i, j;
 	for(i=l;i<=r;i++){
 		if(mn>k[i]){
@@ -20,25 +18,22 @@ void run(int l, int r){
 			mid=i;
 		}
 	}
-	for(i=l;i<mid;i++){
-		suml+=k[i];
-	}
-	for(i=mid+1;i<r;i++){
-		sumr+= k[i];
-	}
 	if(mid==l) run(l+1, r);
 	else if(mid==r) run(l, r-1);
-	else if(suml>sumr) run(l, mid-1);
+	else if(dp[mid-1]-dp[l-1]>dp[r]-dp[mid]) run(l, mid-1);
 	else run(mid+1, r);
 	}
 }
 int main() {
 	Shaenno;
 	cin >> n;
-	for(int i=0;i<n;i++){
+	dp[0]=0;
+	k[0]=0;
+	for(int i=1;i<=n;i++){
 		cin >> k[i];
+		dp[i]=k[i]+dp[i-1];
 	}
-	run(0, n-1);
+	run(1, n);
 	cout << ans <<'\n';
 	return 0;
 }
